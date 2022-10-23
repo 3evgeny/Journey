5b7cb36cc0db77cabeb6f1b1d66b8f4abb112754
melself <maskot2002@yandex.ru>
2022-10-24 02:33:15 +0300

    Create Adapters

---
+++ app/src/main/java/com/melself/journey/ui/Adapters/HotelAdapter.java
@@ -0,0 +1,38 @@
+package com.melself.journey.ui.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.melself.journey.databinding.ListHotelBinding;
+
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.HotelViewHolder> {
+
+    public class HotelViewHolder extends RecyclerView.ViewHolder {
+        ListHotelBinding binding;
+
+        public HotelViewHolder(ListHotelBinding item) {
+            super(item.getRoot());
+            binding = item;
+        }
+    }
+
+    @NonNull
+    @Override
+    public HotelAdapter.HotelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new HotelViewHolder(ListHotelBinding.inflate(LayoutInflater.from(parent.getContext()),parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HotelAdapter.HotelViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
